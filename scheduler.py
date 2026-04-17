af312e776101af6d3ce0c781bb6ef7dfbb4ad938
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 18:24:36 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,46 +1,18 @@
-from api import get_today_matches
-from predictor import analyze_match
-from telegram import Bot
-from config import TELEGRAM_TOKEN, CHAT_ID
+import schedule
+import time
 
-bot = Bot(token=TELEGRAM_TOKEN)
+from scheduler import send_morning_bets
+from scheduler import send_evening_bets
 
-def send_morning_bets():
 
-    matches = get_today_matches()
+# 🟢 3 مباريات فالصباح
+schedule.every().day.at("09:00").do(send_morning_bets)
 
-    bets = []
+# 🔴 3 مباريات فالمساء
+schedule.every().day.at("19:00").do(send_evening_bets)
 
-    for m in matches:
+print("BetBot running...")
 
-        result = analyze_match(m["team1"], m["team2"])
-
-        if result["Confidence"] >= 86:
-
-            bets.append({
-                "team1": m["team1"],
-                "team2": m["team2"],
-                "league": m["league"],
-                "pick": result["Pick"],
-                "confidence": result["Confidence"],
-                "score": result["Score"]
-            })
-
-    bets = sorted(bets, key=lambda x: x["confidence"], reverse=True)
-
-    top3 = bets[:3]
-
-    msg = "🔥 MORNING FIXED BETS\n\n"
-
-    for i, b in enumerate(top3, 1):
-
-        msg += f"""
-{i}) {b['team1']} vs {b['team2']}
-League: {b['league']}
-Pick: {b['pick']}
-Confidence: {b['confidence']}%
-Score: {b['score']}
-
-"""
-
-    bot.send_message(chat_id=CHAT_ID, text=msg)
+while True:
+    schedule.run_pending()
+    time.sleep(60)
