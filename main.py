3bdbb2a1f6b60807608ae065a1b554c1958a2a66
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 21:13:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from bot import create_app
+from telegram.ext import ApplicationBuilder, CommandHandler
+from config import TELEGRAM_TOKEN
+from bot import start
 
-app = create_app()
 
-print("🔥 BETBOT PRO RUNNING...")
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("🔥 BOT RUNNING...")
 
 app.run_polling()
