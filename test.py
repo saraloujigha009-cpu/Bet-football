f60d80066d6dbd36ceed3d30ad0ee0a3e5d3a4a9
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 15:33:08 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-
-TOKEN = "8754438460:AAEliYNcIBCu9eb26oy8b7kXPoEqiHZhuVg"
-CHAT_ID = 8797710776
-
-bot = Bot(token=TOKEN)
-
-bot.send_message(chat_id=CHAT_ID, text="🔥 Test message from BetBot")
