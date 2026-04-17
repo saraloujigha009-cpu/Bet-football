77c0d6c2ef866f3e82b24af1023827a3d5f2f8f4
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 14:52:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from api import get_matches
 
 matches = get_matches()
 
+best_bets = []
+
 for match in matches:
 
     result = predict_match(
@@ -10,5 +12,21 @@ for match in matches:
         match["team2_form"][0]
     )
 
-    print("\n", match["team1"], "vs", match["team2"])
-    print(result)
+    if result["Confidence%"] >= 70:
+        best_bets.append({
+            "match": f"{match['team1']} vs {match['team2']}",
+            "data": result
+        })
+
+# ترتيب من الأقوى للأضعف
+best_bets.sort(key=lambda x: x["data"]["Confidence%"], reverse=True)
+
+# نخليو غير top 1
+if best_bets:
+    top = best_bets[0]
+
+    print("\n🔥 BEST BET TODAY 🔥")
+    print(top["match"])
+    print(top["data"])
+else:
+    print("❌ No strong bets today")
