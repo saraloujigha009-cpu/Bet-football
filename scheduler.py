c2ad51d1b3f3d5807ae3e44b726361bfd1631096
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 17:51:32 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,12 +5,12 @@ from telegram import Bot
 
 TOKEN = "8754438460:AAEliYNcIBCu9eb26oy8b7kXPoEqiHZhuVg"
 CHAT_ID = "8797710776"
+API_KEY = "bc2df9e4ea817767c164df0b13d1f24b"
 
 bot = Bot(token=TOKEN)
 
-API_KEY = "bc2df9e4ea817767c164df0b13d1f24b"
 
-def get_matches():
+def get_matches_today():
 
     today = datetime.today().strftime('%Y-%m-%d')
 
@@ -38,9 +38,10 @@ def get_matches():
     return matches
 
 
-def send_daily_bets():
+# 🟢 3 BETS MORNING
+def send_morning_bets():
 
-    matches = get_matches()
+    matches = get_matches_today()
     bets = []
 
     for m in matches:
@@ -54,11 +55,39 @@ def send_daily_bets():
 
     top3 = bets[:3]
 
-    if not top3:
-        bot.send_message(chat_id=CHAT_ID, text="❌ No strong bets today")
-        return
+    msg = "🔥 MORNING FIXED BETS\n\n"
 
-    msg = "🔥 TODAY FIXED BETS\n\n"
+    for i, (m, r) in enumerate(top3, 1):
+
+        msg += f"""
+{i}) {m['team1']} vs {m['team2']}
+Pick: {r['Pick']}
+Confidence: {r['Confidence%']}%
+Score: {r['Score']}
+
+"""
+
+    bot.send_message(chat_id=CHAT_ID, text=msg)
+
+
+# 🔴 3 LIVE BETS EVENING
+def send_live_bets():
+
+    matches = get_matches_today()
+    bets = []
+
+    for m in matches:
+
+        result = analyze_match(m["team1"], m["team2"], 1.8, 1.6)
+
+        if result["Confidence%"] >= 87:
+            bets.append((m, result))
+
+    bets.sort(key=lambda x: x[1]["Confidence%"], reverse=True)
+
+    top3 = bets[:3]
+
+    msg = "⚡ LIVE BETS EVENING\n\n"
 
     for i, (m, r) in enumerate(top3, 1):
 
