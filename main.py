02c510713fd38ae7bcbc1440b3f58570099b7626
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 18:46:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import schedule
 import time
 
-from scheduler import send_morning_bets, send_live_bets
+from scheduler import send_morning_bets, send_evening_bets
 
+# 🟢 صباح 09:00
 schedule.every().day.at("09:00").do(send_morning_bets)
 
-schedule.every().day.at("19:00").do(send_live_bets)
+# 🔴 مساء 19:00
+schedule.every().day.at("19:00").do(send_evening_bets)
 
-print("BetBot running...")
+print("🔥 BetBot is running 24/7...")
 
 while True:
     schedule.run_pending()
