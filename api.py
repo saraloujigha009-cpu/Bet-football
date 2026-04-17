4933b22f0c3cf7c861c4b2bdc75e8879fcf129c2
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 18:04:22 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,40 +1,28 @@
 import requests
+from datetime import datetime
 
 API_KEY = "bc2df9e4ea817767c164df0b13d1f24b"
 
-headers = {"x-apisports-key": API_KEY}
+def get_today_matches():
 
-def get_form(team_id):
+    today = datetime.today().strftime('%Y-%m-%d')
 
-    url = f"https://v3.football.api-sports.io/fixtures?team={team_id}&last=5"
-    res = requests.get(url, headers=headers)
-    data = res.json()
+    url = f"https://v3.football.api-sports.io/fixtures?date={today}"
 
-    goals_scored = 0
-    goals_conceded = 0
-    wins = 0
+    headers = {
+        "x-apisports-key": API_KEY
+    }
+
+    r = requests.get(url, headers=headers)
+    data = r.json()
+
+    matches = []
 
     for m in data["response"]:
 
-        home_id = m["teams"]["home"]["id"]
-        away_id = m["teams"]["away"]["id"]
+        matches.append({
+            "team1": m["teams"]["home"]["name"],
+            "team2": m["teams"]["away"]["name"]
+        })
 
-        home_goals = m["goals"]["home"]
-        away_goals = m["goals"]["away"]
-
-        if team_id == home_id:
-            goals_scored += home_goals
-            goals_conceded += away_goals
-            if home_goals > away_goals:
-                wins += 1
-        else:
-            goals_scored += away_goals
-            goals_conceded += home_goals
-            if away_goals > home_goals:
-                wins += 1
-
-    return {
-        "avg_scored": goals_scored / 5,
-        "avg_conceded": goals_conceded / 5,
-        "win_rate": wins / 5
-    }
+    return matches
