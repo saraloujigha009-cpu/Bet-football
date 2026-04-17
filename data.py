d46156e81efce042790e1ca4f0c93c488ae3f677
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 14:20:35 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+matches = [
+    {
+        "team1": "Team A",
+        "team2": "Team B",
+        "team1_avg_goals": 1.6,
+        "team2_avg_goals": 1.3
+    },
+    {
+        "team1": "Team C",
+        "team2": "Team D",
+        "team1_avg_goals": 0.9,
+        "team2_avg_goals": 1.8
+    }
+]
