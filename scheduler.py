25d6201f1d69437f6d4a46b166ad66545d90db7b
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 18:43:57 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,46 @@
-import schedule
-import time
+from api import get_today_matches, get_team_stats
+from predictor import analyze_match
+from telegram import Bot
+from config import TELEGRAM_TOKEN, CHAT_ID
 
-from scheduler import send_morning_bets
-from scheduler import send_evening_bets
+bot = Bot(token=TELEGRAM_TOKEN)
 
 
-# 🟢 3 مباريات فالصباح
-schedule.every().day.at("09:00").do(send_morning_bets)
+def send_morning_bets():
 
-# 🔴 3 مباريات فالمساء
-schedule.every().day.at("19:00").do(send_evening_bets)
+    matches = get_today_matches()
+    bets = []
 
-print("BetBot running...")
+    for m in matches:
 
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+        stats1 = get_team_stats(m["team1_id"])
+        stats2 = get_team_stats(m["team2_id"])
+
+        result = analyze_match(m["team1"], m["team2"], stats1, stats2)
+
+        if result["Confidence"] >= 85:
+
+            bets.append({
+                "team1": m["team1"],
+                "team2": m["team2"],
+                "league": m["league"],
+                "pick": result["Pick"],
+                "confidence": result["Confidence"],
+                "score": result["Score"]
+            })
+
+    bets = sorted(bets, key=lambda x: x["confidence"], reverse=True)[:3]
+
+    msg = "🔥 MORNING FIXED BETS\n\n"
+
+    for i, b in enumerate(bets, 1):
+
+        msg += f"""
+{i}) {b['team1']} vs {b['team2']}
+League: {b['league']}
+Pick: {b['pick']}
+Confidence: {b['confidence']}%
+Score: {b['score']}
+"""
+
+    bot.send_message(chat_id=CHAT_ID, text=msg)
