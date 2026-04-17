faeda3651a32776f9913d7a1f32063691de74112
saraloujigha009-cpu <saraloujigha009@gmail.com>
2026-04-17 14:50:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from predictor import predict_match
 from api import get_matches
-from api import get_team_form
 
 matches = get_matches()
 
 for match in matches:
 
-    team1_form = get_team_form(match["team1_id"])
-    team2_form = get_team_form(match["team2_id"])
-
     result = predict_match(
-        team1_form[0],
-        team2_form[0]
+        match["team1_form"][0],
+        match["team2_form"][0]
     )
 
-    print(f"{match['team1']} vs {match['team2']} => {result}")
+    print("\n", match["team1"], "vs", match["team2"])
+    print(result)
